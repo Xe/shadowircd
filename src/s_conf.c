e62255621d453cd58cdbf4bdff33f59a271f2467
Niichan <niichan@ponychat.net>
2013-04-14 21:55:19 -0700

    Make flood control settings configurable by those who know exactly what they're doing.
    atheme/charybdis/commit/e6e54763d9654d6c1012172d5af6cc3b4a0e40a3

--- src/s_conf.c
+++ src/s_conf.c
@@ -812,7 +812,6 @@ set_default_conf(void)
 	ConfigFileEntry.min_nonwildcard_simple = 3;
 	ConfigFileEntry.default_floodcount = 8;
 	ConfigFileEntry.default_ident_timeout = 5;
-	ConfigFileEntry.client_flood = CLIENT_FLOOD_DEFAULT;
 	ConfigFileEntry.tkline_expire_notices = 0;
 
         ConfigFileEntry.reject_after_count = 5;
@@ -820,6 +819,13 @@ set_default_conf(void)
 	ConfigFileEntry.reject_duration = 120;
 	ConfigFileEntry.throttle_count = 4;
 	ConfigFileEntry.throttle_duration = 60;
+	
+	ConfigFileEntry.client_flood_max_lines = CLIENT_FLOOD_DEFAULT; 
+	ConfigFileEntry.client_flood_burst_rate = 5; 
+	ConfigFileEntry.client_flood_burst_max = 5; 
+	ConfigFileEntry.client_flood_message_time = 1;
+	ConfigFileEntry.client_flood_message_num = 2; 
+	
 	ConfigFileEntry.expire_override_time = 300;
 
 	ServerInfo.default_max_clients = MAXCONNECTIONS;
@@ -887,9 +893,9 @@ validate_conf(void)
 				
 	}
 
-	if((ConfigFileEntry.client_flood < CLIENT_FLOOD_MIN) ||
-	   (ConfigFileEntry.client_flood > CLIENT_FLOOD_MAX))
-		ConfigFileEntry.client_flood = CLIENT_FLOOD_MAX;
+	if((ConfigFileEntry.client_flood_max_lines < CLIENT_FLOOD_MIN) ||
+	   (ConfigFileEntry.client_flood_max_lines > CLIENT_FLOOD_MAX))
+		ConfigFileEntry.client_flood_max_lines = CLIENT_FLOOD_MAX;
 
 	if(!split_users || !split_servers ||
 	   (!ConfigChannel.no_create_on_split && !ConfigChannel.no_join_on_split))
