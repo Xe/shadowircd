3a7e173246b975571970300df06e88198abb1abc
Niichan <niichan@ponychat.net>
2013-05-22 11:33:05 -0700

    Updated extensions/extb_channel to allow for private channels

--- extensions/extb_modchannel.c
+++ extensions/extb_modchannel.c
@@ -1,6 +1,6 @@
 /*
- * Channel extban type: matches users who are in a certain channel
- * -- jilles
+ * Moderation channel extban type: matches users who are in a certain private channel
+ * -- Niichan
  *
  */
 
@@ -13,14 +13,14 @@
 
 static int _modinit(void);
 static void _moddeinit(void);
-static int eb_channel(const char *data, struct Client *client_p, struct Channel *chptr, long mode_type);
+static int eb_modchannel(const char *data, struct Client *client_p, struct Channel *chptr, long mode_type);
 
 DECLARE_MODULE_AV1(extb_channel, _modinit, _moddeinit, NULL, NULL, NULL, "$Revision: 1723 $");
 
 static int
 _modinit(void)
 {
-	extban_table['c'] = eb_channel;
+	extban_table['m'] = eb_modchannel;
 
 	return 0;
 }
@@ -28,10 +28,10 @@ _modinit(void)
 static void
 _moddeinit(void)
 {
-	extban_table['c'] = NULL;
+	extban_table['m'] = NULL;
 }
 
-static int eb_channel(const char *data, struct Client *client_p,
+static int eb_modchannel(const char *data, struct Client *client_p,
 		struct Channel *chptr, long mode_type)
 {
 	struct Channel *chptr2;
@@ -45,6 +45,9 @@ static int eb_channel(const char *data, struct Client *client_p,
 		return EXTBAN_INVALID;
 	/* require consistent target */
 	if (chptr->chname[0] == '#' && data[0] == '&')
+		return EXTBAN_INVALID;
+	/* privacy! don't allow +s/+p channels to influence another channel */
+	if (PubChannel(chptr2))
 		return EXTBAN_INVALID;
 	return IsMember(client_p, chptr2) ? EXTBAN_MATCH : EXTBAN_NOMATCH;
 }
