59d6bc5f1809c43e1156e5e1dc775d77d28d859a
Niichan <niichan@ponychat.net>
2013-05-22 11:54:58 -0700

    Revert "remove redundant testing code"
    This reverts commit f47b353df119b76bc3230ec0ba1a801c3417330c.

---
+++ extensions/extb_modchannel.c
@@ -0,0 +1,53 @@
+/*
+ * Moderation channel extban type: matches users who are in a certain private channel
+ * -- Niichan
+ *
+ */
+
+#include "stdinc.h"
+#include "modules.h"
+#include "client.h"
+#include "channel.h"
+#include "hash.h"
+#include "ircd.h"
+
+static int _modinit(void);
+static void _moddeinit(void);
+static int eb_modchannel(const char *data, struct Client *client_p, struct Channel *chptr, long mode_type);
+
+DECLARE_MODULE_AV1(extb_channel, _modinit, _moddeinit, NULL, NULL, NULL, "$Revision: 1723 $");
+
+static int
+_modinit(void)
+{
+	extban_table['m'] = eb_modchannel;
+
+	return 0;
+}
+
+static void
+_moddeinit(void)
+{
+	extban_table['m'] = NULL;
+}
+
+static int eb_modchannel(const char *data, struct Client *client_p,
+		struct Channel *chptr, long mode_type)
+{
+	struct Channel *chptr2;
+
+	(void)chptr;
+	(void)mode_type;
+	if (data == NULL)
+		return EXTBAN_INVALID;
+	chptr2 = find_channel(data);
+	if (chptr2 == NULL)
+		return EXTBAN_INVALID;
+	/* require consistent target */
+	if (chptr->chname[0] == '#' && data[0] == '&')
+		return EXTBAN_INVALID;
+	/* privacy! don't allow +s/+p channels to influence another channel */
+	if (PubChannel(chptr2))
+		return EXTBAN_INVALID;
+	return IsMember(client_p, chptr2) ? EXTBAN_MATCH : EXTBAN_NOMATCH;
+}
